ace48926c72bf1eaf9ff3d1f3738e9fb184ac562
VincentChen <nippon1218@163.com>
2019-08-23 23:51:02 +0800

    Initial Commit

---
+++ Observer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Observer
+//
+//  Created by Vincent on 2019/8/23.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
