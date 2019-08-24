692bf5f58f49df77d766b16afea7008fb461f9f9
VincentChen <nippon1218@163.com>
2019-08-24 23:45:16 +0800

    add observer.cpp

---
+++ Observer/Observer.cpp
@@ -0,0 +1,9 @@
+//
+//  Observer.cpp
+//  Observer
+//
+//  Created by Vincent on 2019/8/24.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+#include "Observer.hpp"
---
+++ Observer/Observer.hpp
@@ -0,0 +1,14 @@
+//
+//  Observer.hpp
+//  Observer
+//
+//  Created by Vincent on 2019/8/24.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+#ifndef Observer_hpp
+#define Observer_hpp
+
+#include <stdio.h>
+
+#endif /* Observer_hpp */
